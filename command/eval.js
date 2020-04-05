77382ea14042192a14fad5cd66beeba454c0123b
stephen <54988149+sharoskyy@users.noreply.github.com>
2020-04-05 19:19:20 +0300

    don't let this command to everyone, put your id.

---
+++ command/eval.js
@@ -0,0 +1,20 @@
+const Discord = require("discord.js");
+const db = require("quick.db")
+
+exports.run = async (bot, message, args, color, prefix) => {
+    if (message.author.id !== 'yourID') return message.channel.send("no perms.")
+    try {
+        let codein = args.join(" ");
+        let code = eval(codein);
+
+        if (typeof code !== 'string')
+            code = require('util').inspect(code, { depth: 0 });
+        let embed = new Discord.RichEmbed()
+        .setColor('#469425')
+        .addField(':inbox_tray: Input', `\`\`\`js\n${codein}\`\`\``)
+        .addField(':outbox_tray: Output', `\`\`\`js\n${code}\n\`\`\``)
+        message.channel.send(embed)
+    } catch(e) {
+        message.channel.send(`\`\`\`js\n${e}\n\`\`\``);
+    }
+};
