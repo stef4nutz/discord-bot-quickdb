fbfa558281f9b54b1f1cc30fd69809e35658655a
stephen <54988149+sharoskyy@users.noreply.github.com>
2020-07-11 15:18:26 +0300

    Update profile.js

--- commands/profile.js
+++ commands/profile.js
@@ -5,15 +5,11 @@ exports.run = async(bot, message, args) => {
   
   let member;
   if(args.join(' ')) member = message.mentions.members.first() || message.guild.members.get(args[0]) || message.guild.members.filter(m => m.user.username.toLowerCase().includes(args.join(' ').toLowerCase())).first()
-  if(!args.join(' ') || member === undefined) member = message.member;
-  
-  let gender = await db.fetch(`gender_${member.user.id}`)
-    if(gender === null) gender = 'It has no gender set.'
-    if(gender === false) gender = 'Male'
-    if(gender === true) gender = 'Female'
+  if(!args.join(' ') || member === undefined) member = message.member;    
+    
   let varsta = db.get(`varsta_${member.user.id}`)
   if (!varsta) varsta = "It has no age set.";
-  message.channel.send(member.user.tag + "'s profile\n```Name: " + member.user.tag + "\nAge: " + varsta + "\nGender: " + gender + "```\n")
+  message.channel.send(member.user.tag + "'s profile\n```Name: " + member.user.tag + "\nAge: " + varsta + "```\n")
 }
 exports.conf = {
   enabled: true,
